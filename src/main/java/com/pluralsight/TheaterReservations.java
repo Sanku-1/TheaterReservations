b530e0deb78e0e58e167e0dc1c421343faa8bb9e
Stephen_A <ankustephen99@gmail.com>
2025-09-30 10:04:14 -0400

    Initial Commit

---
+++ src/main/java/com/pluralsight/TheaterReservations.java
@@ -0,0 +1,20 @@
+package com.pluralsight;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class TheaterReservations {
+    public static void main(String[] args) {
+        Scanner reservationScanner = new Scanner(System.in);
+        System.out.println("Please enter your full name:");
+        String nameInput = reservationScanner.nextLine();
+        System.out.println("Please enter the day you would like to watch the movie (MM/dd/yyyy):");
+        String dateInput = reservationScanner.nextLine();
+        System.out.println("Please enter the number of tickets you would like to purchase:");
+        int ticketInput = reservationScanner.nextInt();
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        LocalDate movieDate = LocalDate.parse(dateInput, dateFormatter);
+        System.out.println(movieDate);
+    }
+}
